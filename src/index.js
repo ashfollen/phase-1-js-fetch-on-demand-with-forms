d81bbec134d89774d582e5abf1fcacc2aa6b744b
Ashley Follen <ashleyfollen@outlook.com>
2021-07-26 19:03:24 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-const init = () => {
-  
+//to make sure the JS we write executes when the DOM is fully loaded, any code related to DOM manipulation should either go in init or in a function called within init
+
+const init = () => { 
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID'); //retrieves the value of what we type into the form
+
+        fetch(`http://localhost:3000/movies/${input.value}`)//modify our URL based on the data typed into our form
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+
